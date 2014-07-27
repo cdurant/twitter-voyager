ccb828c29fc753e9f44170f8a21e0c65387d1535
Colin <colin@padopolis.com>
2014-07-27 07:49:24 -0700

    You can display the tweet now.

--- twitter-voyager/client/views.js
+++ twitter-voyager/client/views.js
@@ -2,5 +2,6 @@ Template.counter.count = function () {
 	return Tweets.find().count();
 };
 Template.tweetList.tweet = function () {
-	return Tweets.findOne();
+	var tweet = Tweets.findOne();
+	return tweet;
 };
