776598846f8845e565877f8f38a010d4982dd607
Colin <colin@padopolis.com>
2014-07-27 11:21:24 -0700

    Making some changes.

--- twitter-voyager/client/views.js
+++ twitter-voyager/client/views.js
@@ -5,3 +5,11 @@ Template.tweetList.tweet = function () {
 	var tweet = Tweets.findOne();
 	return tweet;
 };
+Template.controlPanel.events = {
+	'click .start-collecting': function(event) {
+		Meteor.call('startStream');
+	},
+	'click .stop-collecting': function(event) {
+		Meteor.call('stopStream');
+	}
+}
--- twitter-voyager/server/tweet-stream.js
+++ twitter-voyager/server/tweet-stream.js
@@ -1,7 +1,5 @@
 var Fiber = Npm.require('fibers');
 
-var Yo = Npm.require('yo-api');
-
 twit = new TwitMaker({
 	consumer_key: Meteor.settings.twitterApiKey,
 	consumer_secret: Meteor.settings.twitterApiSecret,
@@ -9,14 +7,64 @@ twit = new TwitMaker({
 	access_token_secret: Meteor.settings.twitterAccessSecret
 });
 
-Meteor.startup(function () {
-	var stream = twit.stream('statuses/sample', {'language': 'en'});	
+Meteor.startup(function() {
+	stream = undefined;
+	
+	voyager = new Voyager('voyagerjs.com', 'eb5ae6915390c578364ab299');
+	
+	voyager.on('saveMe', function(eventId,eventData) {
 
-	stream.on('tweet', function(tweet) {
-		Fiber(function() {
-			Tweets.insert ({
-				text: tweet.text
-				});
-		}).run();
-	});
-});
+		console.log(eventData.direction, eventData.currentValue);
+
+		if (eventData.direction === 'over') {
+			
+			voyager.log('error', "Threshold triggered: stream stopped and tweets deleted.")
+
+			Meteor.call('stopStream');
+
+			Meteor.call('removeAllTweets');
+		}
+
+		voyager.eventCompleted(eventId);
+	})
+})
+
+Meteor.methods({
+	startStream: function() {
+
+		voyager.log('debug', 'We\'re trying to start the stream!')
+
+		if (!stream) {
+
+			voyager.log('debug', 'Starting the stream.')
+
+			stream = twit.stream('statuses/sample', {'language': 'en'});	
+
+			stream.on('tweet', function(tweet) {
+				Fiber(function() {
+					
+					if (Math.random() > 0.5) {
+						Tweets.insert ({
+						text: tweet.text
+						});
+					}
+				}).run();
+			});
+		} else {
+			voyager.log('debug', 'Using the restart.')
+			stream.start();
+		}
+	},
+	stopStream: function() {
+
+		voyager.log('debug', 'Stopping stream')
+
+		if (!!stream) {
+			voyager.log('debug', 'Actually stopping')
+			stream.stop();
+		}
+	},
+	removeAllTweets: function() {
+		Tweets.remove({});
+	}
+})
